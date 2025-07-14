03dafcac3cf930d9ae7b3b07d484c7ca24672cad
Abdo-Hammad <abdohammad2030@gmail.com>
2025-07-14 23:06:36 +0300

    Product featur

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute";
+import { seedInitialProducts } from "./services/productService";
+import productRoute from "./routes/productRoute";
 
 
 
 const app = express();
 const port = 3001;
 
+app.use("/product", productRoute)
 app.use(express.json())
 
 mongoose
@@ -14,7 +17,7 @@ mongoose
     .then(() => console.log("Mongo connected!"))
     .catch((err) => console.log("Failed to conncet", err));
 
-
+    seedInitialProducts();
      app.use('/user', userRoute)
 
   app.listen(port, () => {
